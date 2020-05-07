9a701cbe6e6aa67ae31f0aed5f41a10d0b283a10
Mitchel Meskes <2115621@talnet.nl>
2020-05-07 12:20:43 +0200

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,5 +1,17 @@
-ALTER TABLE planeten ADD (diameter INT(255), afstand INT(255), massa DECIMAL(65));
+use sterrenstelsel;
+CREATE TABLE sterrenstelsel.planeten
+(   
+    id INT(200) NOT NULL AUTO_INCREMENT,
+    naam VARCHAR(200) NOT NULL,
+    diameter INT(200) NOT NULL,
+    afstand INT(200) NOT NULL,
+    massa DECIMAL(60,1)  NOT NULL,
+    tijd DATE NULL,
+PRIMARY KEY (id));
+TRUNCATE planeten;
 
-INSERT INTO planeten(naam , diameter, afstand, massa) VALUES ('Zon', '1392000', '0','332946'), ('Mercurius','4880','57910000','0.1'), ('Venus','12104','108208930','0.9'), ('Aarde','12756','149597870','1'), ('Mars','6794','227936640','0.1');
+INSERT INTO planeten(id,naam,diameter,afstand,massa,tijd) 
 
+VALUES 
+('','Zon', '1392000', '0','332946','1111-09-09'),('','Mercurius','4880','57910000','0.1','3333-10-23'),('','Venus','12104','108208930','0.9','1000-11-11'),('','Aarde','12756','149597870','1','0000-00-00'),('','Mars','6794','227936640','0.1','5567-11-14'),('','Mars','6794','227936640','0.1','5567-11-14');
 select * from planeten;
